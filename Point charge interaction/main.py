c603704b5a951a2e277e07c66373f1d0e6d3e790
NoahSchiro <noahschiro@rocketmail.com>
2021-09-07 20:53:35 -0400

    Refactored physics engine
    Whole bunch of changes
    
    - Math is more succinct
    - Electric fields can now handle an arbitrary amount of charges
    - Physics is more vector based (and accurate to real life) instead of willy nilly half-assed functions
    
    Additionally, I have changed the files in point charge interaction to be copies of "Point charges". Changes soon to come

--- Point charge interaction/main.py
+++ Point charge interaction/main.py
@@ -1,4 +1,5 @@
-from classes import *
+from electric_field import *
+from physics_engine import *
 import math
 
 # Constant variables
@@ -8,3 +9,20 @@ scene.height = 800
 # Create field
 my_field = electric_field()
 my_charge = point_charge(0.001, -1E-9, 10, 10, 10)
+
+while True:
+
+    # Set frame rate
+    rate(20)
+
+    # This allows the charge to bounce around
+    if my_charge.pos[0] >= 10:
+        my_charge.apply_force([-0.1, -0.1, -0.1])
+    if my_charge.pos[0] <= -10:
+        my_charge.apply_force([0.1, 0.1, 0.1])
+
+    # Calculates the influence of the charge on space
+    my_field.calculate_field([my_charge])
+
+    # Moves the charge
+    my_charge.update_position()
--- Point charges/main.py
+++ Point charges/main.py
@@ -1,4 +1,5 @@
-from classes import *
+from electric_field import *
+from physics_engine import *
 import math
 
 # Constant variables
@@ -10,13 +11,18 @@ my_field = electric_field()
 my_charge = point_charge(0.001, -1E-9, 10, 10, 10)
 
 while True:
+
+    # Set frame rate
     rate(20)
 
+    # This allows the charge to bounce around
     if my_charge.pos[0] >= 10:
         my_charge.apply_force([-0.1, -0.1, -0.1])
     if my_charge.pos[0] <= -10:
         my_charge.apply_force([0.1, 0.1, 0.1])
 
-    my_field.point_to_charge(my_charge)
-    my_field.change_magnitude(my_charge)
+    # Calculates the influence of the charge on space
+    my_field.calculate_field([my_charge])
+
+    # Moves the charge
     my_charge.update_position()
